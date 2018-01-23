890d6d0b1604a4f95906c643d9713b147b7625e0
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 18:04:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,9 @@ def oxford_comma(array)
   
   if array.length > 2 
     last_elem = array.pop 
-    array.join(" , ")
+    new_string = array.join(" , ")
+    new_string << " and #{last_elem}"
   end 
+  new_string
   
 end
