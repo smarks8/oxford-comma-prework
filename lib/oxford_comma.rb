6412cdf494b2d9a6b324edc65cd0876a5123577e
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 17:56:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,22 +1,6 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join 
-  end 
-  
-  if array.length == 2 
-    array.join( "and" )
-  end 
-  
-  if array.length > 2 
-    new_string = ""
-    for element in array 
-      if element != array.last
-        new_string << array.join(", ")
-      else 
-        new_string << " and #{array.last}"
-      end 
-    end 
-    new_string
-  end 
+  end
   
 end
