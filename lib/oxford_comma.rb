183e30e9ad27db3b976ff0c8fdaae905be58bec5
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 17:58:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
-    array.join 
+    return array.join 
   end
   
   if array.length == 2 
-    array.join(" and ")
+    return array.join(" and ")
   end 
   
 end
